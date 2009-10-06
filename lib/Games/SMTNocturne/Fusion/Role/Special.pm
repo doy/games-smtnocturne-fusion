72308e8b8be71b433eb213d4be732a16f2d5095e
Jesse Luehrs <doy@tozt.net>
2009-10-06 16:42:36 -0500

    add data for special fusions, still unimplemented

--- lib/Games/SMTNocturne/Fusion/Role/Special.pm
+++ lib/Games/SMTNocturne/Fusion/Role/Special.pm
@@ -4,3 +4,75 @@ use namespace::autoclean;
 with 'Games::SMTNocturne::Fusion::Role::NotNormallyFusable';
 
 1;
+
+__DATA__
+---
+Amaterasu:
+  demon1:
+  - Yatagarasu
+  demon2:
+  - Mikazuchi
+  sacrifice:
+  - Uzume
+Gabriel:
+  demon1:
+  - Throne
+  demon2:
+  - Raphael
+Girimehkala:
+  sacrifice:
+  - type
+  - Vile
+  target:
+  - Purski
+Gurr:
+  sacrifice:
+  - type
+  - Tyrant
+  target:
+  - Sparna
+Metatron:
+  demon1:
+  - Michael
+  demon2:
+  - type
+  - - Divine
+    - Seraph
+  sacrifice:
+  - type
+  - Tyrant
+Michael:
+  demon1:
+  - Uriel
+  demon2:
+  - Gabriel
+  demon3:
+  - Raphael
+Ongyo-ki:
+  demon1:
+  - Kin-Ki
+  demon2:
+  - Sui-Ki
+  demon3:
+  - Fuu-Ki
+Raphael:
+  demon1:
+  - Dominion
+  demon2:
+  - Uriel
+Sakahagi:
+  fusion_type:
+  - Element
+  target:
+  - Sakahagi
+Samael:
+  sacrifice:
+  - type
+  - Vile
+  target:
+  - Throne
+Shiva:
+  demon1:
+  - Ragnda
+  demon2:
+  - Barong
